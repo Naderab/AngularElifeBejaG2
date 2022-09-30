dfdadf88b9756656aca36416ecbdc4ba0cd1eb04
Naderab <abdellaouinader@gmail.com>
2022-09-30 16:07:56 +0100

    form part

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../core/product';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-
+  product : Product = new Product();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.product);
   }
 
 }
